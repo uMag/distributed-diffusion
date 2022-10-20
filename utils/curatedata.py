47134c1ffe1861ea775aa1bd9a14dad89d98afab
chavinlo <85657083+chavinlo@users.noreply.github.com>
2022-10-20 09:34:09 -0500

    add curatedata.py

---
+++ utils/curatedata.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+from pathlib import Path
+
+parser = argparse.ArgumentParser(description='Dataset Curator')
+parser.add_argument('--dataset', type=str, default=None, help='Path to dataset')
+args = parser.parse_args()
+
+image_formats = ('.jpg','.jpeg','.png')
+
+pathlist = Path(args.dataset).rglob('*')
+for path in pathlist:
+    extension = os.path.splitext(path)[1]
+    if extension == ".txt":
+        filename = os.path.splitext(path)[0]
+        x = os.path.isfile(str(filename) + ".jpg") or os.path.isfile(str(filename) + ".png") or os.path.isfile(str(filename) + ".jpeg")
+        if x is False:
+            os.remove(path)
+    elif extension in image_formats:
+        filename = os.path.splitext(path)[0]
+        x = os.path.isfile(str(filename) + ".txt")
+        if x is False:
+            os.remove(path)
