ba58990532ca0bbf3927181f84e4943f33d57cd8
chavinlo <85657083+chavinlo@users.noreply.github.com>
2022-10-29 15:27:59 -0500

    test 2

--- utils/dataset_processing/convert_dataset.py
+++ utils/dataset_processing/convert_dataset.py
@@ -128,6 +128,18 @@ class AspectDataset(torch.utils.data.Dataset):
 
         return return_dict
 
+    
+    def collate_fn(self, examples):
+            pixel_values = torch.stack([example['pixel_values'] for example in examples if example is not None])
+            pixel_values.to(memory_format=torch.contiguous_format).float()
+            input_ids = [example['input_ids'] for example in examples if example is not None]
+            padded_tokens = self.tokenizer.pad({'input_ids': input_ids}, return_tensors='pt', padding=True)
+            return {
+                'pixel_values': pixel_values,
+                'input_ids': padded_tokens.input_ids,
+                'attention_mask': padded_tokens.attention_mask,
+            }
+
 dataset = AspectDataset(store, tokenizer)
 
 class AspectBucket:
