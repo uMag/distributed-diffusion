687ed8b502093a7d9019859cb5bf4c641df1d468
chavinlo <85657083+chavinlo@users.noreply.github.com>
2022-11-18 21:25:07 -0500

    fix server so its public

--- utils/dataset/online/server/main.py
+++ utils/dataset/online/server/main.py
@@ -175,5 +175,5 @@ backgroundTask1 = BackgroundTasks()
 backgroundTask1.start()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080, host="0.0.0.0")
     
